9895a4d6869e54ec81ef63c28a12877c65b26c68
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-09-28 23:48:04 +1000

    show current user's email in dashboard view

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+  const [error, setError] = useState("");
+  const { currentUser } = useAuth();
+
+  const handleLogout = () => {};
+
+  return (
+    <>
+      <Card style={{ padding: "1em" }}>
+        <h2 className="text-center mb-4">Profile</h2>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <p>
+          <strong>Email:</strong> {currentUser.email}
+        </p>
+        <p>
+          <Link to="update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </p>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
