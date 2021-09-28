8288029c0581aa0e023d03da0e975559b15c69ba
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-09-29 00:03:53 +1000

    add logout method and logout button in dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,27 +1,39 @@
 import { useState } from "react";
 import { Card, Button, Alert } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 const Dashboard = () => {
   const [error, setError] = useState("");
-  const { currentUser } = useAuth();
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
 
-  const handleLogout = () => {};
+  const handleLogout = async () => {
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  };
 
   return (
     <>
-      <Card style={{ padding: "1em" }}>
-        <h2 className="text-center mb-4">Profile</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <p>
-          <strong>Email:</strong> {currentUser.email}
-        </p>
-        <p>
-          <Link to="update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </p>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <p>
+            <strong>Email:</strong> {currentUser.email}
+          </p>
+          <p>
+            <Link to="update-profile" className="btn btn-primary w-100 mt-3">
+              Update Profile
+            </Link>
+          </p>
+        </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
         <Button variant="link" onClick={handleLogout}>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -6,28 +6,30 @@ const AuthContext = React.createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-	const [currentUser, setCurrentUser] = useState();
-	const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
-	const signup = (email, password) =>
-		auth.createUserWithEmailAndPassword(email, password);
+  const signup = (email, password) =>
+    auth.createUserWithEmailAndPassword(email, password);
 
-	const login = (email, password) =>
-		auth.signInWithEmailAndPassword(email, password);
+  const login = (email, password) =>
+    auth.signInWithEmailAndPassword(email, password);
 
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged(user => {
-			setCurrentUser(user);
-			setLoading(false);
-		});
-		return unsubscribe;
-	}, []);
+  const logout = () => auth.signOut();
 
-	const value = { currentUser, signup, login };
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
 
-	return (
-		<AuthContext.Provider value={value}>
-			{!loading && children}
-		</AuthContext.Provider>
-	);
+  const value = { currentUser, signup, login, logout };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 };
