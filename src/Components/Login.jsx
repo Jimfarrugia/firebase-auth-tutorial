a282937112d18eaafe163d09a6c1e4962a75385c
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-09-29 00:04:13 +1000

    formatting

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,51 +4,53 @@ import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 const Login = () => {
-	const emailRef = useRef();
-	const passwordRef = useRef();
-	const { login } = useAuth();
-	const [error, setError] = useState("");
-	const [loading, setLoading] = useState(false);
-	const history = useHistory();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
-	const handleSubmit = async e => {
-		e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		try {
-			setError("");
-			setLoading(true);
-			await login(emailRef.current.value, passwordRef.current.value);
-			history.push("/");
-		} catch {
-			setError("Failed to sign in.");
-		}
-		setLoading(false);
-	};
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
+    } catch {
+      setError("Failed to sign in.");
+    }
+    setLoading(false);
+  };
 
-	return (
-		<>
-			<Card style={{ padding: "1em" }}>
-				<h2 className="text-center mb-4">Log In</h2>
-				{error && <Alert variant="danger">{error}</Alert>}
-				<Form onSubmit={handleSubmit}>
-					<Form.Group id="email" style={{ paddingBottom: "1em" }}>
-						<Form.Label>Email</Form.Label>
-						<Form.Control type="email" ref={emailRef} required />
-					</Form.Group>
-					<Form.Group id="password" style={{ paddingBottom: "1em" }}>
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" ref={passwordRef} required />
-					</Form.Group>
-					<Button disabled={loading} className="w-100" type="submit">
-						Log In
-					</Button>
-				</Form>
-			</Card>
-			<div className="w-100 text-center mt-2">
-				Need an account? <Link to="/signup">Sign Up</Link>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email" style={{ paddingBottom: "1em" }}>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password" style={{ paddingBottom: "1em" }}>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Log In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
+  );
 };
 
 export default Login;
